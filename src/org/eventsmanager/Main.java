9431ba0e1486151ff0675781da88a6e330a9e7f1
LeonardoFlammini <leonardo98flammini@gmail.com>
2024-05-27 10:29:35 +0200

    Milestone 2

--- src/org/eventsmanager/Main.java
+++ src/org/eventsmanager/Main.java
@@ -1,7 +1,44 @@
 package org.eventsmanager;
 
+import java.time.LocalDate;
+import org.eventsmanager.exceptions.InvalidDateException;
+
 public class Main {
     public static void main(String[] args) {
+
+        try {
+
+            Event event = new Event("Concert", LocalDate.of(2024, 12, 25), 100);
+
+
+            System.out.println("Event created: " + event);
+
+
+            event.book(10);
+            System.out.println("Reserved 10 seats.");
+            System.out.println("Event status after booking 10 seats: " + event);
+
+
+            event.book(90);
+            System.out.println("Reserved 90 seats.");
+            System.out.println("Event status after booking 90 seats: " + event);
+
+
+            event.cancelBooking(5);
+            System.out.println("Canceled 5 seats.");
+            System.out.println("Event status after cancelling 5 seats: " + event);
+
+            //Genero un' eccezione
+            event.cancelBooking(100);
+
+        } catch (InvalidDateException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("An unexpected error occurred: " + e.getMessage());
+        }
+
 
     }
 }
